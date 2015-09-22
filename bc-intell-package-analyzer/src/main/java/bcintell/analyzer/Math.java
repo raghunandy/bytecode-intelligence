4f361d9f8b0be5e03ff9234331926cc5f45a98af
Raghunath <visumagic@gmail.com>
2015-09-21 20:02:02 -0500

    test cases

---
+++ bc-intell-package-analyzer/src/main/java/bcintell/analyzer/Math.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bcintell.analyzer;
+
+/**
+ *
+ * @author Raghu
+ */
+public class Math {
+    
+    public static int add(int a,int b){
+        return a+b;
+    }
+}
