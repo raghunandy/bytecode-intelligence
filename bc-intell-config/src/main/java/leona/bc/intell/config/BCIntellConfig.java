d2688c4fe103480ef83d3264852505c2b2ab27ea
Raghunath <visumagic@gmail.com>
2015-09-29 11:15:53 -0500

    email generator  setup

---
+++ bc-intell-config/src/main/java/leona/bc/intell/config/BCIntellConfig.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leona.bc.intell.config;
+
+/**
+ *
+ * @author Raghu
+ */
+public class BCIntellConfig {
+    
+}
