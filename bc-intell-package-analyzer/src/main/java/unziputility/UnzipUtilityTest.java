3d2cc8aa72ad958131fa430f71df30c1f73043e5
Piya <Piya@10.161.7.101>
2015-10-03 18:22:32 -0500

    Unzipping/extracting zip files

---
+++ bc-intell-package-analyzer/src/main/java/unziputility/UnzipUtilityTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unziputility;
+
+/**
+ *
+ * @author Piya
+ */
+public class UnzipUtilityTest {
+    public static void main(String[] args) {
+        String zipFilePath = "/Users/Piya/desktop/Cover Letter.zip";
+        String destDirectory = "/Users/Piya/desktop";
+        UnzipUtility unzipper = new UnzipUtility();
+        try {
+            unzipper.unzip(zipFilePath, destDirectory);
+        } catch (Exception ex) {
+            // if some errors occurred
+            ex.printStackTrace();
+        }
+    }
+}
