42977b3c5accf1fb6da2be5d8cab5fb5cb0ee331
Raghunath <visumagic@gmail.com>
2015-10-30 19:54:47 -0500

    correcting extract jar

--- bc-intell-package-analyzer/src/main/java/bcintell/extractjar/ExtractJar.java
+++ bc-intell-package-analyzer/src/main/java/bcintell/extractjar/ExtractJar.java
@@ -5,8 +5,14 @@
  */
 package bcintell.extractjar;
 
-import bcintell.bc.intell.core.BCELUtil;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 
 /**
@@ -15,69 +21,48 @@ import org.apache.commons.io.FilenameUtils;
  */
 public class ExtractJar {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws java.io.IOException {
+    public static final String DEST_DIR = "extractedJars";
+
+    public static String extractJar(String jarFile) throws IOException {
         // TODO code application logic here
 
-        java.util.jar.JarFile jarfile = new java.util.jar.JarFile(new java.io.File("E:\\BE\\Persistent Project\\BEProjectDeploy\\x86\\BEProjectLibraries_Latest.jar"));
-        java.util.Enumeration<java.util.jar.JarEntry> enu = jarfile.entries();
+        JarFile jarfile = new JarFile(new File(jarFile));
+        Enumeration<JarEntry> enu = jarfile.entries();
         String name = "";
-        String destdir = "ExtractedJars";  //Desktop is my destination directory
+        //Desktop is my destination directory
         String name1 = jarfile.getName();
         String jarname = FilenameUtils.getBaseName(name1);
-        destdir = destdir + "\\" + jarname;
-        new File(destdir).mkdirs();
+        File destdir = new File(DEST_DIR + "\\" + jarname);
+        if (destdir.exists()) {
+            FileUtils.deleteDirectory(destdir);
+        }
+        destdir.mkdirs();
 
         while (enu.hasMoreElements()) {
 
-            java.util.jar.JarEntry file = (java.util.jar.JarEntry) enu.nextElement();
-            java.io.File f = new java.io.File(destdir + "\\" + file.getName());
+            JarEntry file = (JarEntry) enu.nextElement();
+            File f = new File(destdir + "\\" + file.getName());
             name = file.getName();
 
             if (file.isDirectory()) { // if its a directory, create it
                 f.mkdir();
                 continue;
             }
-            java.io.InputStream is = jarfile.getInputStream(file); // get the input stream
-            java.io.FileOutputStream fos = new java.io.FileOutputStream(f);
+            
+            InputStream is = jarfile.getInputStream(file); // get the input stream
+            FileOutputStream fos = new FileOutputStream(f);
             while (is.available() > 0) {  // write contents of 'is' to 'fos'
                 fos.write(is.read());
             }
             fos.close();
             is.close();
-            System.out.println(f.getAbsolutePath());
-            if (f.getName().endsWith(".class") == true) {   //check if extracted file is a class file
-                BCELUtil.inspectMethods(f.getAbsolutePath());   //if it is a class file, submit to BCELUtil for inspection
-            }
-
+            //System.out.println(f.getAbsolutePath());
+//            if (f.getName().endsWith(".class") == true) {   //check if extracted file is a class file
+//               BCELUtil.inspectMethods(f.getAbsolutePath());   //if it is a class file, submit to BCELUtil for inspection
+//            }
         }
-
-       // System.out.print(name);
-        /*java.util.jar.JarEntry je = enu.nextElement();
-
-         System.out.println(je.getName());
-
-         java.io.File fl = new java.io.File(destdir, je.getName());
-         if(!fl.exists())
-         {
-         fl.getParentFile().mkdirs();
-         fl = new java.io.File(destdir, je.getName());
-         }
-         if(je.isDirectory())
-         {
-         continue;
-         }
-         java.io.InputStream is = jarfile.getInputStream(je);
-         java.io.FileOutputStream fo = new java.io.FileOutputStream(fl);
-         while(is.available()>0)
-         {
-         fo.write(is.read());
-         }
-         fo.close();
-         is.close();
-         }*/
+        jarfile.close();
+        return destdir.getAbsolutePath();
     }
 
 }
---
+++ bc-intell-package-analyzer/src/test/java/bcintell/extractjar/ExtractJarTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bcintell.extractjar;
+
+import java.io.File;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Raghu
+ */
+public class ExtractJarTest {
+    
+    public ExtractJarTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of extractJar method, of class ExtractJar.
+     */
+    @Test
+    public void testExtractJar() throws Exception {
+        System.out.println("extractJar");
+        String jarFile = "..\\project-inception\\target\\project-inception-0.1.jar";
+        System.out.println("Current Path:"+new File(".").getAbsolutePath());
+        System.out.println("Input Jar Abs Path:"+new File(jarFile).getAbsolutePath());
+        System.out.println("Extracted To: "+ExtractJar.extractJar(jarFile));
+        
+        
+    }
+    
+}
